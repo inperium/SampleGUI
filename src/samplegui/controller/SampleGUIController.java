3e0bc980f8f66241b14eacb721a45e16579d5f17
inperium <ibow9416@740s-pl-111.local>
2016-11-01 12:19:58 -0600

    There is now a center frame.

--- src/samplegui/controller/SampleGUIController.java
+++ src/samplegui/controller/SampleGUIController.java
@@ -1,12 +1,27 @@
 package samplegui.controller;
 
-public class SampleGUIController
-{
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import samplegui.view.FirstFrame;
 
+public class SampleGUIController extends JFrame
+{
+	private FirstFrame appFrame;
+	
+	public SampleGUIController()
+	{
+		this.appFrame = new FirstFrame(this);
+	}
 	public void start()
 	{
 		
-		
+		this.setupFrame();
 	}
 
+	private void setupFrame()
+	{
+		this.setSize(new Dimension(600,400));
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
 }
---
+++ src/samplegui/view/FirstPanel.java
@@ -0,0 +1,9 @@
+package samplegui.view;
+
+import javax.swing.JPanel;
+import samplegui.controller.SampleGUIController;
+
+public class FirstPanel extends JPanel
+{
+
+}

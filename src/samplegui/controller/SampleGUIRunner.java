f083cc7e3d2206a224e1633c7ea541b9b58629a2
inperium <ibow9416@740s-pl-111.local>
2016-11-01 13:34:44 -0600

    Functional random color generator.

--- src/samplegui/controller/SampleGUIRunner.java
+++ src/samplegui/controller/SampleGUIRunner.java
@@ -1,5 +1,8 @@
 package samplegui.controller;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class SampleGUIRunner
 {
 	public static void main(String[] args)
--- src/samplegui/view/FirstPanel.java
+++ src/samplegui/view/FirstPanel.java
@@ -1,8 +1,12 @@
 package samplegui.view;
 
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.*;
+
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.concurrent.ThreadLocalRandom;
+
 import samplegui.controller.SampleGUIController;
 
 public class FirstPanel extends JPanel
@@ -10,33 +14,40 @@ public class FirstPanel extends JPanel
 	private SampleGUIController baseController;
 	private JButton colorButton;
 	private JButton randomButton;
-	
+	private SpringLayout baseLayout;
+
 	public FirstPanel(SampleGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
-		this.colorButton = new JButton("Access Color");
+		this.baseLayout = new SpringLayout();
+		this.colorButton = new JButton("Reset");
 		this.randomButton = new JButton("New Color");
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	
+
 	private void setupPanel()
 	{
-		this.setBackground(java.awt.Color.LIGHT_GRAY);
+		this.setLayout(baseLayout);
+		this.setBackground(new Color(56,56,56));
 		this.add(colorButton);
 		this.add(randomButton);
 	}
-	
+
 	private void setupLayout()
 	{
-		
+
+		baseLayout.putConstraint(SpringLayout.SOUTH, randomButton, -38, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, randomButton, -210, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, colorButton, 0, SpringLayout.NORTH, randomButton);
+		baseLayout.putConstraint(SpringLayout.WEST, colorButton, 6, SpringLayout.EAST, randomButton);
 	}
-	
+
 	private void setupListeners()
 	{
-		
+		randomButton.addActionListener((event) -> setBackground(new Color(ThreadLocalRandom.current().nextInt(0, 255), ThreadLocalRandom.current().nextInt(0, 255), ThreadLocalRandom.current().nextInt(0, 255))));
+		colorButton.addActionListener((event) -> setBackground(new Color(56,56,56)));
 	}
 }
